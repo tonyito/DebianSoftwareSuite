088151785fb1ce0bc7a9a471d24adf073c48c6dc
tonyito <tonyitocole@gmail.com>
2020-05-02 19:24:16 -0700

    Added basic intro and read feature to initialize script

--- SoftwareSuite.sh
+++ SoftwareSuite.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # Ubuntu Software Suite for Newly Formatted Systems
 
-echo "Please select the package you would like to install"
+echo "Hi! Welcome to the Ubuntu Software Suite installer. This installer includes the following packages: Google Chrome, Discord, Slack, Authy, Postman, and Skype. You can select which ones you would like to install throughout the process.";
+
+echo "Type y to continue, n to exit.";
+
+read INITIALIZE;
+
+if [ $INITIALIZE == "n" ]
+then
+   exit 0; 
+fi
 
 CHROME=false;
+
+
 
 if [ $CHROME == true ]
 then
