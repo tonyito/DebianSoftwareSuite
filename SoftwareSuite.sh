289ac9c07019ef9f554b309ad09874c3c400d03e
tonyito <tonyitocole@gmail.com>
2020-05-02 19:14:25 -0700

    Basic conditional for Chrome added

--- SoftwareSuite.sh
+++ SoftwareSuite.sh
@@ -2,3 +2,12 @@
 # Ubuntu Software Suite for Newly Formatted Systems
 
 echo "Please select the package you would like to install"
+
+CHROME=false;
+
+if [ $CHROME == true ]
+then
+    echo "Installing Chrome..."
+else
+    echo "Not Installing Chrome."
+fi
