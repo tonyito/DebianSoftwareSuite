4b0547a85e0678697ad2cd0f46efce7ff3e7d5ea
tonyito <tonyitocole@gmail.com>
2020-05-02 20:02:51 -0700

    Chrome installation basic option made

--- SoftwareSuite.sh
+++ SoftwareSuite.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 # Ubuntu Software Suite for Newly Formatted Systems
 
-echo "Hi! Welcome to the Ubuntu Software Suite installer. This installer includes the following packages: Google Chrome, Discord, Slack, Authy, Postman, and Skype. You can select which ones you would like to install throughout the process.";
+echo "Hi! Welcome to the Ubuntu Software Suite installer. This installer includes the following packages: Google Chrome, Discord, Slack, Authy, Postman, and Skype. You can select which ones you would like to install throughout the process."
 
-echo "Type y to continue, n to exit.";
+echo "Type a to install all, y to continue with options, n to exit."
 
-read INITIALIZE;
+read INITIALIZE
 
-if [ $INITIALIZE == "n" ]
-then
-   exit 0; 
+if [ $INITIALIZE == "n" ]; then
+    exit 0
 fi
 
-CHROME=false;
+CHROMEOPTION="0"
 
+while [ $CHROMEOPTION != "n" -o $CHROMEOPTION != "y" ]; 
+do
+    echo "Would you like to install chrome? y/n"
 
+    read CHROMEOPTION
 
-if [ $CHROME == true ]
-then
+    if [ $CHROMEOPTION == "n" ]; then
+        CHROME=false;
+        break;
+    elif [ $CHROMEOPTION == "y" ]; then
+        CHROME=true
+        break;
+    else
+        echo "Could not register option $CHROMEOPTION. Please type y/n."
+    fi
+done
+
+if [ $CHROME == true ]; then
     echo "Installing Chrome..."
 else
     echo "Not Installing Chrome."
