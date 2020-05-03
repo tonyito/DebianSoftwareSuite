6cf32b5d5010111a4009cab8b16b8a74de1a3aff
tonyito <tonyitocole@gmail.com>
2020-05-02 20:49:11 -0700

    Installer done

--- SoftwareSuite.sh
+++ SoftwareSuite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ubuntu Software Suite Installer for Newly Formatted Systems
 
-echo -e "\e[36mHi! Welcome to the Ubuntu Software Suite installer. This installer includes the following packages: Chrome Browser, Discord, Slack, Authy, VSCode, Postman, Signal, and Skype. You can select which ones you would like to install throughout the process. nIf you are running Ubuntu <16.04, you must install snap first by running 'sudo apt install snapd'.\e[0m"
+echo -e "\e[36mHi! Welcome to the Unofficial Ubuntu Software Suite installer. This installer includes the following packages: Chrome Browser, Discord, Slack, Authy, VSCode, Postman, Signal, and Skype. You can select which ones you would like to install throughout the process. If you are running Ubuntu <16.04, you must install snap first by running 'sudo apt install snapd'.\e[0m"
 
 echo ""
 
@@ -129,3 +129,122 @@ if [ $AUTHY == true ]; then
 else
     echo "Not Installing Authy."
 fi
+
+VSCODEOPTION="0"
+
+if [ $INITIALIZE != "a" ]; then
+    while [ $VSCODEOPTION != "n" -o $VSCODEOPTION != "y" ]; do
+        echo -e "\e[32mWould you like to install VSCode? y/n\e[0m"
+
+        read VSCODEOPTION
+
+        if [ $VSCODEOPTION == "n" ]; then
+            VSCODE=false
+            break
+        elif [ $VSCODEOPTION == "y" ]; then
+            VSCODE=true
+            break
+        else
+            echo "Could not register option $VSCODEOPTION. Please type y/n."
+        fi
+    done
+else
+    VSCODE=true
+fi
+
+if [ $VSCODE == true ]; then
+    echo "Installing The Latest Version of VSCode..."
+    sudo snap install --classic code
+else
+    echo "Not Installing VSCode."
+fi
+
+POSTMANOPTION="0"
+
+if [ $INITIALIZE != "a" ]; then
+    while [ $POSTMANOPTION != "n" -o $POSTMANOPTION != "y" ]; do
+        echo -e "\e[32mWould you like to install Postman? y/n\e[0m"
+
+        read POSTMANOPTION
+
+        if [ $POSTMANOPTION == "n" ]; then
+            POSTMAN=false
+            break
+        elif [ $POSTMANOPTION == "y" ]; then
+            POSTMAN=true
+            break
+        else
+            echo "Could not register option $POSTMANOPTION. Please type y/n."
+        fi
+    done
+else
+    POSTMAN=true
+fi
+
+if [ $POSTMAN == true ]; then
+    echo "Installing The Latest Version of Postman..."
+    sudo snap install postman
+else
+    echo "Not Installing Postman."
+fi
+
+SIGNALOPTION="0"
+
+if [ $INITIALIZE != "a" ]; then
+    while [ $SIGNALOPTION != "n" -o $SIGNALOPTION != "y" ]; do
+        echo -e "\e[32mWould you like to install Signal? y/n\e[0m"
+
+        read SIGNALOPTION
+
+        if [ $SIGNALOPTION == "n" ]; then
+            SIGNAL=false
+            break
+        elif [ $SIGNALOPTION == "y" ]; then
+            SIGNAL=true
+            break
+        else
+            echo "Could not register option $SIGNALOPTION. Please type y/n."
+        fi
+    done
+else
+    SIGNAL=true
+fi
+
+if [ $SIGNAL == true ]; then
+    echo "Installing The Latest Version of Signal..."
+    sudo snap install signal-desktop
+else
+    echo "Not Installing Signal."
+fi
+
+SKYPEOPTION="0"
+
+if [ $INITIALIZE != "a" ]; then
+    while [ $SKYPEOPTION != "n" -o $SKYPEOPTION != "y" ]; do
+        echo -e "\e[32mWould you like to install Skype? y/n\e[0m"
+
+        read SKYPEOPTION
+
+        if [ $SKYPEOPTION == "n" ]; then
+            SKYPE=false
+            break
+        elif [ $SKYPEOPTION == "y" ]; then
+            SKYPE=true
+            break
+        else
+            echo "Could not register option $SKYPEOPTION. Please type y/n."
+        fi
+    done
+else
+    SKYPE=true
+fi
+
+if [ $SKYPE == true ]; then
+    echo "Installing The Latest Version of Skype..."
+    wget https://go.skype.com/skypeforlinux-64.deb
+    sudo apt install ./skypeforlinux-64.deb
+else
+    echo "Not Installing Skype."
+fi
+
+echo -e "\e[32mAll Done!\e[0m"
