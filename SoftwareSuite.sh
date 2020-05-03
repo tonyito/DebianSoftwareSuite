f23d2d3da4f78fac2ed79062a028578bf30d11b6
tonyito <tonyitocole@gmail.com>
2020-05-02 20:18:28 -0700

    Chrome Browser installation working

--- SoftwareSuite.sh
+++ SoftwareSuite.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Ubuntu Software Suite for Newly Formatted Systems
 
-echo "Hi! Welcome to the Ubuntu Software Suite installer. This installer includes the following packages: Google Chrome, Discord, Slack, Authy, Postman, and Skype. You can select which ones you would like to install throughout the process."
+echo -e "\e[36mHi! Welcome to the Ubuntu Software Suite installer. This installer includes the following packages: Chrome Browser, Discord, Slack, Authy, VSCode, Postman, Signal, and Skype. You can select which ones you would like to install throughout the process.\e[0m";
 
-echo "Type a to install all, y to continue with options, n to exit."
+echo "";
+
+echo -e "\e[32mType a to install all, y to continue with options, n to exit.\e[0m"
 
 read INITIALIZE
 
@@ -13,25 +15,31 @@ fi
 
 CHROMEOPTION="0"
 
-while [ $CHROMEOPTION != "n" -o $CHROMEOPTION != "y" ]; 
-do
-    echo "Would you like to install chrome? y/n"
+if [ $INITIALIZE != "a" ]; then
+while [ $CHROMEOPTION != "n" -o $CHROMEOPTION != "y" ]; do
+    echo -e "\e[32mWould you like to install chrome? y/n\e[0m"
 
     read CHROMEOPTION
 
     if [ $CHROMEOPTION == "n" ]; then
-        CHROME=false;
-        break;
+        CHROME=false
+        break
     elif [ $CHROMEOPTION == "y" ]; then
         CHROME=true
-        break;
+        break
     else
         echo "Could not register option $CHROMEOPTION. Please type y/n."
     fi
 done
+else
+    CHROME=true;
+fi
 
 if [ $CHROME == true ]; then
-    echo "Installing Chrome..."
+    echo "Installing The Latest Version of Chrome..."
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb 
 else
     echo "Not Installing Chrome."
 fi
+
